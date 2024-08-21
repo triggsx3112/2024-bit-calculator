52ca1bf59cc246a069fb8012f5a49ebf134b6185
triggsx3112 <triggsx3112@masseyhigh.school.nz>
2024-08-21 14:23:26 +1200

    working statement generator

---
+++ C_01_Statement_Generator.py
@@ -0,0 +1,7 @@
+# functions go here
+def statement_generator(statement, decoration):
+    print(f"\n{decoration * 5} {statement} {decoration * 5}")
+
+
+# main routine goes here
+statement_generator("instructions", "-")
